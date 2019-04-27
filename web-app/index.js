403c9f4634df6b53e9c969e859fd0b77420211a6
rohit-roy <rohit-roy@rohit-roy.local>
2019-04-27 17:19:00 +0530

    Eslint added

--- web-app/index.js
+++ web-app/index.js
@@ -1,9 +1,13 @@
+/*eslint-disable prop-types*/
 import React from 'react';
+/*eslint-disable prop-types*/
 import ReactDOM from 'react-dom';
 
 const title = 'React Boilerplate Setup';
 
 ReactDOM.render(
   <div>{title}</div>,
+  /*eslint-disable no-undef*/
   document.getElementById('app')
+  /*eslint-disable no-undef*/
 );
