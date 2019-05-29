a1bc0d8c2fc55c3c9d98b1a9fff3422ab8e1fe73
rohit-roy <rohit-roy@rohit-roy.local>
2019-05-30 00:46:01 +0530

    Added support for type script

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -1,34 +1,44 @@
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './web-app/index.js',
+  entry: './web-app/index.tsx',
   module: {
     rules: [
       {
+        test: /\.(ts|tsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              /* Loader options go here */
+            },
+          },
+        ],
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
-      }
-    ]
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.ts', '.tsx', '.js', '.jsx'],
   },
   output: {
     path: __dirname + '/build',
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
     contentBase: './build',
-    hot: true
-  }
+    hot: true,
+  },
 };
---
+++ web-app/index.tsx
@@ -0,0 +1,6 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import Counter from './src/components/Counter';
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
